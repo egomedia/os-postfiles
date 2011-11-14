4a4d2c95f0a8e0a9defaaa0e66e694d01d74ee88
Oli Salisbury <oli@egomedia.co.uk>
2011-11-14 17:27:00 +0000

    init

---
+++ os-postfiles.php
@@ -0,0 +1,165 @@
+<?php
+/*
+Plugin Name: OS Post Files
+Description: Automatically lists all the attached files for a particular post in a user-friendly manner.
+Version: 0.1
+Author: Oli Salisbury
+*/
+
+//Config
+$pages_in = array(22);
+$posts_in = array(0);
+
+/*
+DO NOT EDIT BELOW HERE
+*/
+
+//hooks
+if (WP_ADMIN) {
+	add_action('admin_menu', 'os_postfiles_init');
+	add_action('admin_head', 'os_postfiles_css');
+	add_action('admin_head', 'os_postfiles_init_js');
+	add_action('admin_footer', 'os_postfiles_init_footer');
+}
+
+//function to add to posts
+function addtoindvpost($thispostid, $array) {
+	if (sizeof($array)<0) { $array = array(); }
+	$ins = false;
+	foreach ($array as $id) {
+		if	($id > 0) { $ins = true; break; } 
+	}
+	//returns
+	if ($ins && in_array($thispostid, $array)) { //if there are ins defined and this post is in ins
+		return true;
+	} else if (in_array("-$thispostid", $array) || in_array(0, $array)) { //if this post is suppressed or all posts are suppressed then hide
+		return false;
+	} else if (!$ins) { //if ins arent defined show all
+		return true;
+	} else {
+		return false;
+	}
+}
+
+//function to get all file attachments for a post
+function get_attached_files($post_id=NULL) {
+	global $post;
+	//set default
+	if (!$post_id) { $post_id = $post->ID; }
+	//make query
+	$q['post_type'] = 'attachment';
+	$q['post_mime_type'] = 'application';
+	$q['numberposts'] = -1;
+	$q['post_parent'] = $post->ID;
+	$q['orderby'] = 'menu_order';
+	$q['order'] = 'ASC';
+	//return query
+	return get_posts($q);
+}
+
+//Inserts HTML for meta box, including all existing attachments
+function os_postfiles_add() { 
+	global $post;
+	echo '<div id="os_postfiles">';
+	echo '<div id="os_postfiles_buttons">';
+	echo '<a href="media-upload.php?post_id='.$post->ID.'&TB_iframe=1" class="button button-highlighted thickbox">Upload File</a>';
+	echo ' <a href="#" id="os_postfiles_ajaxclick" class="button">Refresh</a>';
+	echo ' <span id="os_postfiles_loading" style="display:none;"><img src="images/loading.gif" alt="Loading..." style="width:auto; height:auto; margin:0; vertical-align:middle" /></span>';
+	echo '</div>';
+	echo '<div id="os_postfiles_alert"><em>Don\'t forget to click "Refresh" once you\'ve uploaded or edited your files</em></div>';
+	echo '<ul id="os_postfiles_ajax">';
+	$attachments = get_attached_files();
+	foreach ($attachments as $obj) {
+		$file_type = str_replace("application/", "", $obj->post_mime_type);
+		echo '<li class="widget">';
+		echo '<b class="';
+		echo $file_type=='pdf' || $file_type=='msword' ? $file_type : '';
+		echo '"></b> ';
+		echo '<strong>'.$obj->post_title.'</strong>';
+		echo '<a href="media-upload.php?post_id='.$post->ID.'&tab=gallery&TB_iframe=1" class="thickbox edit-post-attachment" rel="'.$post->ID.'">Edit</a>';
+		echo '</li>';
+	}
+	echo '</ul>';
+	echo '<div style="clear:both;"></div>';
+	echo '</div>';
+}
+
+//Creates meta box on all Posts and Pages
+function os_postfiles_metabox() {
+	global $_GET, $pages_in, $posts_in;
+	//for custom post types
+	add_meta_box('os_postfiles_list', 'Attachments', 'os_postfiles_add', '');
+	//pages
+	if (addtoindvpost($_GET['post'], $pages_in)) {
+		add_meta_box('os_postfiles_list', 'Attachments', 'os_postfiles_add', 'page');
+	}
+	//posts
+	if (addtoindvpost($_GET['post'], $posts_in)) {
+		add_meta_box('os_postfiles_list', 'Attachments', 'os_postfiles_add', 'post');
+	}
+}
+
+//javascript in header
+function os_postfiles_init_js() {
+	echo '
+	<script type="text/javascript" charset="utf-8">
+	jQuery(document).ready(function(){
+		//hide redundant boxes
+		jQuery("#gallery-settings").hide();
+		jQuery("tr.url").hide();
+		jQuery("tr.align").hide();
+		jQuery("tr.image-size").hide();
+		jQuery("td.savesend input").hide();
+		//ajax update
+		jQuery("#os_postfiles_ajaxclick").click(function() {
+			jQuery("#os_postfiles_alert").hide();
+			jQuery("#os_postfiles_ajax").slideUp("fast", function() { jQuery("#os_postfiles_loading").show(); }).load("'.$_SERVER['REQUEST_URI'].' #os_postfiles_ajax", function() { 
+				jQuery(this).slideDown("fast");
+				jQuery("#os_postfiles_loading").hide();
+			});
+			return false;
+		});
+		//alert
+		jQuery(".thickbox").live("click", function(){ jQuery("#os_postfiles_alert").show(); });
+	});
+	</script>';
+}
+
+//javascript in footer
+function os_postfiles_init_footer() {
+}
+
+//plugin css
+function os_postfiles_css() {
+	echo '
+	<style type="text/css">
+	#os_postfiles { padding:10px 10px 0 10px; }
+	#os_postfiles_buttons { margin-bottom:20px; }
+	#os_postfiles ul { margin:0; padding:0; list-style:none; }
+	#os_postfiles li { margin:0 10px 6px 0; padding:10px; clear:both; }
+	#os_postfiles li b { display:inline-block; width:30px; height:25px; background:url(http://horizontestserver.co.uk/assets/ico-sprite.png) no-repeat 0 0; vertical-align:middle; }
+	#os_postfiles li b.pdf { background-position:-30px 0; }
+	#os_postfiles li b.msword { background-position:-60px 0; }
+	#os_postfiles li a { float:right; line-height:29px; }
+	#os_postfiles_alert { display:none; position:absolute; top:5px; left:130px; z-index:100; }
+	#os_postfiles_alert em { display:block; background:#fff59b; padding:10px; position:relative; }
+	#os_postfiles_alert em:after { 
+		border-color: #fff59b transparent;
+		border-style: solid;
+		border-width: 10px 10px 0;
+		bottom: -10px;
+		content: "";
+		display: block;
+		left: 10px;
+		position: absolute;
+		width: 0;
+	}
+	</style>';
+}
+
+//initialise plugin
+function os_postfiles_init() {
+	wp_enqueue_script('jquery');
+	os_postfiles_metabox();
+}
+?>
